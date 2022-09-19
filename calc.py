ac8bf92ff0fbe115d1c68e6e75d77b78b41ca4d8
Tomas Borelli <tomasborelli@protonmail.com>
2022-09-19 18:08:14 -0300

    refactoring and validation changes, now all validations are done in calculate_returns method

--- calc.py
+++ calc.py
@@ -3,42 +3,51 @@ DECIMALS = 8
 
 
 class Calculator:
+    """Models a daily-compound calculator."""
     def __init__(self):
+        """Inits calculator with the needed variables."""
         self.annual_rate = None
         self.initial_investment = None
         self.period_q = None
+        self.time_period = None
         self.total_capital = 0
         self.period_interest = 0
         self.period_earns = []
         self.start()
 
     def start(self):
+        """Prompts for all the variables needed to calculate the interests."""
         self.initial_investment = input("Enter your investment amount: ")
         self.annual_rate = input("Enter the annual interest rate: ")
         self.period_q = input("Enter a period of time for your capital: ")
 
     def calculate_returns(self):
-        self.total_capital = float(self.initial_investment)
-        self.period_interest = (float(self.annual_rate) / 100) / YEAR
-        for period_of_time in range(int(self.period_q)):
-            period_gain = self.total_capital * self.period_interest
-            self.period_earns.append(period_gain)
-            self.total_capital += period_gain
-            print(f"Period {period_of_time + 1} out of {self.period_q} earnings: {round(period_gain, DECIMALS)}")
+        """Checks the data provided by the user. If the data is ok, it will
+        continue to calculate and inform the period gains. If data there's
+        a problem with the data it will ask again to complete all the variables.
+        """
+        data_checked = False
+        while not data_checked:
+            try:
+                self.total_capital = float(self.initial_investment)
+                self.period_interest = (float(self.annual_rate) / 100) / YEAR
 
-    def valid_data(self):
-        data_to_check = [self.initial_investment, self.annual_rate, self.period_q]
-        try:
-            for data in data_to_check:
-                float(data)
-            return True
-        except ValueError:
-            print("There was a problem with your data. Please enter only numbers.")
-            return False
+                for period_of_time in range(int(self.period_q)):
+                    period_gain = self.total_capital * self.period_interest
+                    self.period_earns.append(period_gain)
+                    self.total_capital += period_gain
+                    print(f"Period {period_of_time + 1} out of {self.period_q} earnings: {round(period_gain, DECIMALS)}")
+
+                data_checked = True
+
+            except ValueError:
+                print("There was a problem with your data. Please enter only numbers.\n")
+                self.start()
 
     def inform_details(self):
+        """Complete detailed information on earnings."""
         print(f"\nInitial investment: {self.initial_investment}")
         print(f"Annual rate: {self.annual_rate}%")
-        print(f"Period of time: {self.period_q}t")
+        print(f"Period of time: {self.period_q} days")
         print(f"Total earnings: {round(sum(self.period_earns), DECIMALS)}")
         print(f"Gains + initial investment: {round(self.total_capital, DECIMALS)}")
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-"""Script to calculate in details the compound interest rate for a given period of days."""
+"""Script to calculate in details the daily compound interest rate for a given
+period of days.
+"""
 from calc import Calculator
 
 calculator = Calculator()
 
-while not calculator.valid_data():
-    calculator.start()
+
 
 calculator.calculate_returns()
 calculator.inform_details()
